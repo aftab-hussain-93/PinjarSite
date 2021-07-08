2af997a3734605873610a9d7908d811d1b36169c
Aftab <aftab.h.1993@gmail.com>
2021-07-09 01:48:41 +0530

    added cors for api login end points

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,14 +2,30 @@ import { withIronSession } from "next-iron-session";
 import { userCookieName } from '../../utils/contants'
 import NextCors from 'nextjs-cors';
 
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
+
 async function handler(req, res) {
 
-    await NextCors(req, res, {
-        // Options
-        methods: ['GET', 'POST'],
-        origin: '*',
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    });
+    await runMiddleware(req, res, cors)
 
     // get user from database then:
     req.session.set("user", {
